347478b48f3a52d11eca5d3ac4dd59f160c0aa0e
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-04-03 10:54:35 -0500

    Create opcode1.c

---
+++ opcode1.c
@@ -0,0 +1,74 @@
+#include "monty.h"
+
+/**
+ * push - Adds an element to a stack
+ * @stack: stack to push
+ * @line_number: Line number of bytecode
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    stack_t *newNode;
+
+    newNode = malloc(sizeof(stack_t));
+
+    if (!newNode)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+		    free_stack(stack);
+		    err();
+    }
+ 
+    (void)line_number;
+  
+    if (*stack)
+          (*stack)->prev = newNode;
+
+    newNode->prev = NULL;
+	  newNode->next = *stack;
+	  newNode->n = 0;
+	  *stack = newNode;
+}
+
+/**
+ * pop - Removes the first element
+ * @stack: stack to pop
+ * @line_number: number of bytecode file
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+         stack_t *temp;
+         
+         temp = *stack;
+
+         if (!(*stack))
+         {
+              fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		          fclose(file);
+		          free_stack(stack);
+		          exit(EXIT_FAILURE);
+         }
+
+          *stack = temp->next;
+  
+          if (*stack)
+                  (*stack)->prev = NULL;
+          free(temp);
+}
+
+/**
+ * pint - prints the value
+ * @stack: Stack to pint
+ * @line_number: number of bytecode file
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+      if(!(*stack))
+      {
+            fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		        fclose(file);
+		        free_stack(stack);
+		        exit(EXIT_FAILURE);
+      }
+  
+      printf("%d\n", (*stack)->n);
+}
